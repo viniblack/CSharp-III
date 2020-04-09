96dd5cee613152b09598f328e22e8cbb3887067e
Black <viniibl4ck@gmail.com>
2020-04-09 10:33:20 -0300

    Agora a classe diretor recebe a classe funcionario por meio de herança

--- ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -4,15 +4,11 @@ using System.Text;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor
+    public class Diretor : Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
-
         public double GetBonificacao()
         {
-            return Salario * 0.10;
+            return Salario;
         }
     }
 }
